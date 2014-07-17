8a9e2cc2dbb45cb8b755551814e2ea2746c6a343
Weslley Neri <weslley39@gmail.com>
2014-07-17 12:16:07 -0300

    Nova estrutura

---
+++ app.js
@@ -0,0 +1,31 @@
+var express = require('express')
+  , app = express() // Web framework to handle routing requests
+  , cons = require('consolidate') // Templating library adapter for Express
+  , MongoClient = require('mongodb').MongoClient // Driver for connecting to MongoDB
+  , routes = require('./routes'); // Routes for our application
+
+MongoClient.connect('mongodb://localhost:27017/blog', function(err, db) {
+    "use strict";
+    if(err) throw err;
+    var pub = __dirname + '/public',
+        view = __dirname + '/views';
+
+    // Register our templating engine
+    app.engine('html', cons.swig);
+    app.set('view engine', 'html');
+    app.set('views', __dirname + '/views');
+
+    // Express middleware to populate 'req.cookies' so we can access cookies
+    app.use(express.cookieParser());
+
+    // Express middleware to populate 'req.body' so we can access POST variables
+    app.use(express.bodyParser());
+
+    app.use(express.static(pub));
+
+    // Application routes
+    routes(app, db);
+
+    app.listen(8080);
+    console.log('The Magic Happens on port 8080');
+});
